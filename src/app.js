0f97fbe7846878e6221238e3f529dc802a37a2bd
Antberry <aniej2013@gmail.com>
2019-07-01 12:20:07 -0700

    register page and nav bar created but not styled yet

--- src/app.js
+++ src/app.js
@@ -1,20 +1,22 @@
 import React, { Fragment } from 'react';
-import { Route } from 'react-router';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ChatWindow from './chat';
 // import Home from './home';
-// import Register from './register';
-// import Login from './login';
+import Register from './register';
+import Login from './login';
+import AboutUs from './aboutus';
 
 
 function App() {
   return (
-  // <Router>
-  //   <Route exact path='/' component={Home} />
-  //   <Route path='/register' component={Register} />
-  //   <Route path='/login' component={Login} />
-  //   <Route path='/chat' component={ChatWindow} />
-  // </Router>
-  <ChatWindow/>
+  <Router>
+  {/* //   <Route exact path='/' component={Home} /> */}
+    <Route path='/register' component={Register} />
+    <Route path='/login' component={Login} />
+    {/* <Route path='/chat' component={ChatWindow} />  */}
+    {/* <Route path='./aboutus' component ={AboutUs} */}
+  </Router>
   );
 }
 
