81565f517a7b40027f67b946c925a02dbb752e23
Antberry <aniej2013@gmail.com>
2019-07-01 12:20:07 -0700

    register page styled for the most part

--- src/webpages/register.js
+++ src/webpages/register.js
@@ -2,6 +2,8 @@ import React, { Component, Fragment } from 'react';
 import 'crypto-js/lib-typedarrays';
 import { Auth } from "aws-amplify";
 import Navigation from './nav';
+import '../css/register.css';
+import '../css/reset.css';
 
 export default class Register extends Component {
 
@@ -29,54 +31,85 @@ export default class Register extends Component {
       <>
       <Navigation></Navigation>
       <form onSubmit={event => this.onRegister(event)}>
+      <div className="theform">
         <div className="container">
-          <h1>Register</h1>
+          <h2>Register</h2>
           <p>Please fill in this form to create an account.</p>
           <br />
-          <label htmlfor="firstName"><b>First Name</b></label>
-          <input type="text" placeholder="First Name" name="firstName" required />
+         
+          <div className="item1"> 
+            <label htmlfor="firstName"><b>First Name</b></label>
+          </div>
+          <div classname="item2">  
+            <input type="text" name="firstName" required />
+          </div>
 
-          <label htmlfor="lastName"><b>Last Name</b></label>
-          <input type="text" placeholder="Last Name" name="lastName" required />
+          <div className="item3"> 
+            <label htmlfor="lastName"><b>Last Name</b></label>
+            </div>
+            <div className="item4">
+            <input type="text" name="lastName" required />
+          </div>
 
-          <label htmlFor="username"><b>Email</b></label>
-          <input type="text" placeholder="Enter Email" name="username" required />
+          <div className="item5">
+            <label htmlFor="username"><b>Email</b></label>
+            </div>
+            <div className="item6">
+            <input type="text" name="username" required />
+          </div>
 
-          <label htmlFor="password"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="password" required />
+          <div className="item7">
+            <label htmlFor="password"><b>Password</b></label>
+            </div>
+            <div className="item8">
+            <input type="password" name="password" required />
+          </div>
 
-          <label htmlFor="passwordRepeat"><b>Confirm Password</b></label>
-          <input type="password" placeholder="Repeat Password" name="passwordRepeat" required />
+          <div className="item9">
+            <label htmlFor="passwordRepeat"><b>Confirm Password</b></label>
+            </div>
+            <div className="item10">
+            <input type="password" name="passwordRepeat" required />
+          </div>
 
-          <label htmlfor="photo"><b>Profile Pic</b></label>
-          <input type="file" name="myPic"/>
+          <div className="item11">
+            <label htmlFor="langPref"><b>Language Preference</b></label>
+            </div>
+            <div className="item12">
+            <select>
+              <option value="arabic">Arabic</option>
+              <option value="chineseT">Chinese (Traditional)</option>
+              <option value="chineseS">Chinese (Simplified)</option>
+              <option value="czech">Czech</option>
+              <option value="danish">Danish</option>
+              <option value="dutch">Dutch</option>
+              <option value="english">English</option>
+              <option value="finnish">Finnish</option>
+              <option value="french">French</option>
+              <option value="german">German</option>
+              <option value="hebrew">Hebrew</option>
+              <option value="indonesian">Indonesian</option>
+              <option value="italian">Italian</option>
+              <option value="japanese">Japanese</option>
+              <option value="korean">Korean</option>
+              <option value="polish">Polish</option>
+              <option value="portuguese">Portuguese</option>
+              <option value="russian">Russian</option>
+              <option value="spanish">Spanish</option>
+              <option value="swedish">Swedish</option>
+              <option value="turkish">Turkish</option>
+            </select>
+          </div>
 
-          <label htmlFor="langPref"><b>Language Preference</b></label>
-          <select>
-            <option value="arabic">Arabic</option>
-            <option value="chineseT">Chinese `(Traditional)`</option>
-            <option value="chineseS">Chinese `(Simplified)`</option>
-            <option value="czech">Czech</option>
-            <option value="danish">Danish</option>
-            <option value="dutch">Dutch</option>
-            <option value="english">English</option>
-            <option value="finnish">Finnish</option>
-            <option value="french">French</option>
-            <option value="german">German</option>
-            <option value="hebrew">Hebrew</option>
-            <option value="indonesian">Indonesian</option>
-            <option value="italian">Italian</option>
-            <option value="japanese">Japanese</option>
-            <option value="korean">Korean</option>
-            <option value="polish">Polish</option>
-            <option value="portuguese">Portuguese</option>
-            <option value="russian">Russian</option>
-            <option value="spanish">Spanish</option>
-            <option value="swedish">Swedish</option>
-            <option value="turkish">Turkish</option>
-          </select>
+          <div className="item13">
+            <label htmlfor="photo"><b>Profile Pic</b></label>
+            </div>
+            <div className="item14">
+            <input type="file" name="myPic"/>
+          </div>
 
           <button className="registerbtn">Register</button>
+          </div>
         </div>
       </form>
       </>
