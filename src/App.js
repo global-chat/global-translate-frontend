b0dac330f70efd7b9a1ef26cf2c416b254ad438d
Vinh Nguyen <nguyenv2@outlook.com>
2019-07-29 09:40:38 -0700

    updates to incorp routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Route } from 'react-router';
 import ChatWindow from './chat';
+// import Home from './home';
+// import Register from './register';
+// import Login from './login';
+
 
 function App() {
   return (
-    <div className="App">
-      <ChatWindow/>
-    </div>
+  // <Router>
+  //   <Route exact path='/' component={Home} />
+  //   <Route path='/register' component={Register} />
+  //   <Route path='/login' component={Login} />
+  //   <Route path='/chat' component={ChatWindow} />
+  // </Router>
+  <ChatWindow/>
   );
 }
 
