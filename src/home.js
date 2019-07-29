f8fea3a403863e54c468309901b5d20b2e1dc8a0
Charles Clemens <cclemensjr@gmail.com>
2019-07-29 11:50:28 -0700

    Added basic layout

--- src/home.js
+++ src/home.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 // export class Home extends Component {
 //   render() {
@@ -16,9 +17,13 @@ import Row from 'react-bootstrap/Row'
 // export default Home;
 
 export const Home = () => (
-  <div>
-    This is the home pageXOffset;
-  </div>
+  <Container>
+    <Row>
+      <Col xs={12} md={8}>
+        <h1>Home Page</h1>
+      </Col>
+    </Row>
+  </Container>
 )
 
 export default Home;
