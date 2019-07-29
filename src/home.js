cb7e26cbfb2e5a052c7d4dba63dffc2437088d8e
Charles Clemens <cclemensjr@gmail.com>
2019-07-29 11:09:23 -0700

    Added component to home page

--- src/home.js
+++ src/home.js
@@ -1,3 +1,16 @@
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+
+export class Home extends Component {
+  render() {
+    return (
+      <Container>
+
+      </Container>
+    );
+  }
+}
 
 
