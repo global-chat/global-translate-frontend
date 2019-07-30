6d0ac8634311857809b5b8fc2f47d6f31b7c4b6c
Charles Clemens <cclemensjr@gmail.com>
2019-07-30 09:21:30 -0700

    Updated jumbotron

--- src/home.js
+++ src/home.js
@@ -1,57 +1,17 @@
 import React from 'react';
 import Layout from './components/layout'
 import Jumbotron from './components/jumbotron'
+import Card from './components/card'
 import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+
 
 export const Home = () => (
   <Layout>
     <Jumbotron />
     <Row>
-      <Col>
-        <Card /*style={{ width: '18rem' }}*/>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </Col>
-    
-      <Col>
-        <Card /*style={{ width: '18rem' }}*/>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </Col>
-
-      <Col>
-        <Card /*style={{ width: '18rem' }}*/>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </Col>
-
+      <Card />
+      <Card />
+      <Card />
     </Row>
   </Layout>
 )
