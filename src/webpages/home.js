2a68263c98b2605e94f1967007f4edd1cd771848
Charles Clemens <cclemensjr@gmail.com>
2019-07-31 13:07:34 -0700

    Moved content into container

--- src/webpages/home.js
+++ src/webpages/home.js
@@ -3,7 +3,9 @@ import Navigation from './nav';
 import Layout from '../components/layout';
 import Jumbotron from '../components/jumbotron';
 import Card from '../components/card';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import '../css/home.css';
 import '../css/cover.css';
 
@@ -21,13 +23,28 @@ export default class Home extends Component {
             <Card />
           </Row> */}
           <Navigation props={this.props} logout={this.props.logoutUser} userName={this.props.userName} userToken={this.props.userToken}></Navigation>
+          <Container>
           <main role="main" class="inner cover">
-          <h1 class="cover-heading">Cover your page.</h1>
-          <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-          <p class="lead">
-            <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
-          </p>
-        </main>
+            <Row>
+
+              <Col>
+                <h1 class="cover-heading">Cover your page.</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <p class="lead">
+                  <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+                </p>
+              </Col>
+            </Row>
+            </main> 
+          </Container>
         </Layout>
       </Fragment>
 
