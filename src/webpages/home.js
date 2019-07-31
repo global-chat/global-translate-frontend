c1b16d66da1cb34c3c4df6a80744f12f68ac39ca
Charles Clemens <cclemensjr@gmail.com>
2019-07-31 10:36:49 -0700

    Added background image to home page

--- src/webpages/home.js
+++ src/webpages/home.js
@@ -12,22 +12,23 @@ export default class Home extends Component {
   render() {
     return (
       <Fragment>
-        <Navigation props={this.props} logout={this.props.logoutUser} userName={this.props.userName} userToken={this.props.userToken}></Navigation>
-        <main role="main" class="inner cover">
+        
+        <Layout>
+          {/* <Jumbotron />
+          <Row>
+            <Card />
+            <Card />
+            <Card />
+          </Row> */}
+          <Navigation props={this.props} logout={this.props.logoutUser} userName={this.props.userName} userToken={this.props.userToken}></Navigation>
+          <main role="main" class="inner cover">
           <h1 class="cover-heading">Cover your page.</h1>
           <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
           <p class="lead">
             <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
           </p>
         </main>
-        {/* <Layout>
-          <Jumbotron />
-          <Row>
-            <Card />
-            <Card />
-            <Card />
-          </Row>
-        </Layout> */}
+        </Layout>
       </Fragment>
 
     );
